0ffa14f648605bcdce6825022ae7d749e278fd77
Gontarenko Dmitriy <dmitriy.gontarenko@accenture.com>
2018-11-02 15:33:10 +0300

    commit: add config.xml files

--- src/main/java/com/accenture/flowershop/be/entity/user/UserAccount.java
+++ src/main/java/com/accenture/flowershop/be/entity/user/UserAccount.java
@@ -1,7 +1,16 @@
 package com.accenture.flowershop.be.entity.user;
 
+import javax.persistence.*;
+
+//@Entity
+//@Table(name = "EMPLOYEES")
 public class UserAccount {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long ID;
+//    @Column(name = "NAME")
     private String name;
+//    @Column(name = "PASSWORD")
     private String password;
 
     public UserAccount() {
@@ -10,6 +19,14 @@ public class UserAccount {
     public UserAccount(String name, String password) {
         this.name = name;
         this.password = password;
+    }
+
+    public Long getID() {
+        return ID;
+    }
+
+    public void setID(Long ID) {
+        this.ID = ID;
     }
 
     public String getName() {
---
+++ src/main/resources/dbscripts/v2__addus.sql
@@ -0,0 +1,2 @@
+INSERT INTO EMPLOYEES (ID, NAME, PASSWORD)
+              VALUES(2, 'Hello', 'Hello');
